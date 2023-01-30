97903dafb2296aadb8bc001aa56779e4ddb2f149
dooovid <86132881+dooovid@users.noreply.github.com>
2023-01-29 23:45:14 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (e) => e.waitUntil(self.skipWaiting()));
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-
-self.addEventListener("message", (e) => {
-    e.source.postMessage(Date.now());
-    debugger;
-});
